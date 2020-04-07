afc40b85b7cd8681c97e8e9b706b5980806dec1b
jacob <jacob.charles@caseware.com>
2020-04-07 11:57:05 -0400

    update setuptools version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README') as file:
 
 setup(name='tornado-swirl',
       python_requires='>=3.6.0',
-      version='0.1.20',
+      version='0.1.20-cwi',
       url='https://github.com/rduldulao/tornado-swirl',
       zip_safe=False,
       packages=['tornado_swirl'],
